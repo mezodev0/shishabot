561cb97c347e5594b06b2b390a70a12bde451e44
mezodev0 <mezodev02@gmail.com>
2021-12-27 11:51:58 -0800

    fixed small bug

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -18,18 +18,19 @@ async fn setup(ctx: &Context, msg: &Message) -> CommandResult {
         let settings_file = tokio::fs::read_to_string("src/server_settings.json").await?;
         let mut settings: Root = serde_json::from_str(&settings_file)?;
 
+        for mut server in settings.servers.iter_mut() {
+            if server.server_id == msg.guild_id.unwrap().to_string() {
+                server.replay_channel = id1.to_string();
+                server.output_channel = id2.to_string();
+                break;
+            }
+        }
+
         let edited_settings = serde_json::to_string(&settings).unwrap();
         if let Err(why) = tokio::fs::write("src/server_settings.json", edited_settings).await {
             warn!("Failed to edit server specific settings: {}", why);
         }
 
-        for mut server in settings.servers.iter_mut() {
-            if server.server_id == msg.guild_id.unwrap().to_string() {
-                server.replay_channel = id1.to_string();
-                server.replay_channel = id2.to_string();
-                break;
-            }
-        }
         msg.reply(&ctx, "Successfully changed settings!").await?;
     } else {
         msg.reply(&ctx, "You need to mention 2 channels!").await?;
