dbf8c64c3006b15c4457b2b7bd677549c2327002
mezodev0 <mezodev02@gmail.com>
2022-07-19 09:20:45 -0700

    moved settings struct to main.rs

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -16,8 +16,5 @@ pub use skinlist::*;
 mod settings_struct;
 pub use settings_struct::*;
 
-#[path = "../server_settings_struct.rs"]
-pub(crate) mod server_settings_struct;
-
 mod setup;
 pub use setup::*;
