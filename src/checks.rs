35f7a589218726a649c1586c8877fffd907bfc75
mezodev0 <mezodev02@gmail.com>
2022-07-20 04:55:04 -0700

    removed unwraps

--- src/checks.rs
+++ src/checks.rs
@@ -16,21 +16,25 @@ async fn permissions_check(
         .guild_id
         .ok_or_else(|| Reason::Log("message was not sent in a guild".to_string()))?;
 
-    let guild = ctx.cache.guild(guild_id).unwrap();
+    if let Some(guild) = ctx.cache.guild(guild_id) {
+        if let Some(channel) = &ctx.cache.guild_channel(msg.channel_id) {
+            let member = match guild_id.member(&ctx, &msg.author.id).await {
+                Ok(member) => member,
+                Err(err) => return Err(Reason::Log(format!("couldn't fetch member: {err}"))),
+            };
 
-    let perms = guild
-        .user_permissions_in(
-            &ctx.cache.guild_channel(msg.channel_id).unwrap(),
-            &msg.guild_id
-                .unwrap()
-                .member(&ctx, msg.author.id)
-                .await
-                .unwrap(),
-        )
-        .unwrap();
+            let perms = match guild.user_permissions_in(channel, &member) {
+                Ok(perms) => perms,
+                Err(err) => return Err(Reason::Log(format!("couldn't fetch permissions: {err}"))),
+            };
 
-    if perms.contains(Permissions::ADMINISTRATOR) || perms.contains(Permissions::MANAGE_CHANNELS) {
-        return Ok(());
+            if !perms
+                .intersection(Permissions::ADMINISTRATOR | Permissions::MANAGE_CHANNELS)
+                .is_empty()
+            {
+                return Ok(());
+            }
+        }
     }
 
     Err(Reason::User(
