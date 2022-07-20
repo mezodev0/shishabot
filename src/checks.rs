0a037e4074b8a3e966761c28455bbb17b339b69c
mezodev0 <mezodev02@gmail.com>
2022-07-20 09:57:56 -0700

    added debug for check

--- src/checks.rs
+++ src/checks.rs
@@ -17,26 +17,29 @@ async fn permissions_check(
         .ok_or_else(|| Reason::Log("message was not sent in a guild".to_string()))?;
 
     if let Some(guild) = ctx.cache.guild(guild_id) {
+        info!("guild passed");
         if let Some(channel) = &ctx.cache.guild_channel(msg.channel_id) {
+            info!("channel passed");
             let member = match guild_id.member(&ctx, &msg.author.id).await {
                 Ok(member) => member,
                 Err(err) => return Err(Reason::Log(format!("couldn't fetch member: {err}"))),
             };
-
+            info!("member passed");
             let perms = match guild.user_permissions_in(channel, &member) {
                 Ok(perms) => perms,
                 Err(err) => return Err(Reason::Log(format!("couldn't fetch permissions: {err}"))),
             };
-
+            info!("perms passed");
             if !perms
                 .intersection(Permissions::ADMINISTRATOR | Permissions::MANAGE_CHANNELS)
                 .is_empty()
             {
+                info!("ok");
                 return Ok(());
             }
         }
     }
-
+    info!("err");
     Err(Reason::User(
         "Lacking required permission to run command".to_string(),
     ))
