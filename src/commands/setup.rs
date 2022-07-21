cfc9a8da22a387f6e331a0570fd9f17937039b08
mezodev0 <mezodev02@gmail.com>
2022-07-20 17:41:43 -0700

    setup now displays the assigned channels

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -1,6 +1,10 @@
+use std::collections::hash_map::Entry;
+
 use crate::checks::PERMISSIONS_CHECK;
 use crate::{server_settings_struct::Server, ServerSettings};
 use anyhow::{Context, Error};
+use serenity::builder::ParseValue;
+use serenity::utils::Color;
 use serenity::{
     client::Context as SerenityContext,
     framework::standard::{macros::command, CommandResult},
@@ -19,13 +23,13 @@ async fn setup(ctx: &SerenityContext, msg: &Message) -> CommandResult {
         .split_whitespace()
         .filter_map(|arg| arg.parse::<ChannelId>().ok());
 
+    let mut data = ctx.data.write().await;
+    let settings = data.get_mut::<ServerSettings>().unwrap();
+
     if let (Some(id1), Some(id2)) = (mentioned_channels.next(), mentioned_channels.next()) {
         let guild_id = msg.guild_id.unwrap_or_default();
 
         let edited_settings = {
-            let mut data = ctx.data.write().await;
-            let settings = data.get_mut::<ServerSettings>().unwrap();
-
             settings
                 .servers
                 .entry(guild_id)
@@ -48,8 +52,43 @@ async fn setup(ctx: &SerenityContext, msg: &Message) -> CommandResult {
         }
 
         msg.reply(&ctx, "Successfully changed settings!").await?;
-    } else {
-        msg.reply(&ctx, "You need to mention 2 channels!").await?;
+    } else if let Entry::Occupied(o) = settings.servers.entry(msg.guild_id.unwrap_or_default()) {
+        if o.get().output_channel != 0 {
+            msg.channel_id
+                .send_message(&ctx, |m| {
+                    m.reference_message((msg.channel_id, msg.id))
+                        .allowed_mentions(|f| {
+                            f.replied_user(false)
+                                .parse(ParseValue::Everyone)
+                                .parse(ParseValue::Users)
+                                .parse(ParseValue::Roles)
+                        });
+                    m.embed(|e| {
+                        e.title(format!(
+                            "Current channel setup{}",
+                            if let Some(guild) = msg.guild_id {
+                                format!(" for {}", guild.name(&ctx).unwrap_or_default())
+                            } else {
+                                "".to_owned()
+                            }
+                        ))
+                        .description(format!(
+                            "**Input Channel:** <#{}>\n**Output Channel:** <#{}>",
+                            o.get().replay_channel,
+                            o.get().output_channel
+                        ))
+                        .footer(|f| {
+                            {
+                                f.text("Use !!setup [input-channel] [output-channel] to edit this")
+                            }
+                        })
+                        .color(Color::new(15785176))
+                    })
+                })
+                .await?;
+        } else {
+            msg.reply(&ctx, "You need to mention 2 channels!").await?;
+        }
     }
 
     Ok(())
