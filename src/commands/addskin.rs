cc68bcbabe33d79b07af84f2a92860d275fe799d
mezodev0 <mezodev02@gmail.com>
2022-08-30 09:59:27 -0700

    fixed  by looking for  in the skin folder

--- src/commands/addskin.rs
+++ src/commands/addskin.rs
@@ -1,7 +1,7 @@
 use std::{
     fmt::{Display, Formatter},
     io::Cursor,
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use crate::checks::BOTOWNER_CHECK;
@@ -51,6 +51,7 @@ impl Display for FileCounter {
 #[checks(BotOwner)]
 #[description = "**Requires osu! skin attachment**\nAllows you to upload custom skins"]
 async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResult {
+    
     let attachment = match msg.attachments.last() {
         Some(a) if matches!(a.filename.split('.').last(), Some("osk")) => a,
         Some(_) | None => {
@@ -59,6 +60,8 @@ async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResu
             return Ok(());
         }
     };
+
+    let mut resp = msg.reply(&ctx, "Downloading skin...").await?;
 
     let bytes = match attachment.download().await {
         Ok(bytes) => bytes,
@@ -69,6 +72,8 @@ async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResu
             return Ok(());
         }
     };
+
+    resp.edit(&ctx, |m| m.content("Creating archive...")).await?;
 
     let cursor = Cursor::new(bytes);
 
@@ -81,6 +86,8 @@ async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResu
             return Ok(());
         }
     };
+
+    resp.edit(&ctx, |m| m.content("Generating skin name...")).await?;
 
     let skinname = if let Some((filename, _extension)) = attachment.filename.rsplit_once('.') {
         let mut file_count = FileCounter::new(filename.to_string());
@@ -98,6 +105,8 @@ async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResu
         return Ok(());
     };
 
+    resp.edit(&ctx, |m| m.content("Extracting archive...")).await?;
+
     match archive.extract(format!("../Skins/{}", skinname)) {
         Ok(()) => (),
         Err(err) => {
@@ -107,6 +116,35 @@ async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResu
             return Ok(());
         }
     };
+    
+    resp.edit(&ctx, |m| m.content("Checking for `skin.ini`...")).await?;
+
+    if Path::new(&format!("../Skins/{}/skin.ini", &skinname)).exists() {
+        resp.edit(&ctx, |m| m.content("Found `skin.ini`!")).await?;
+    } else {
+        let mut skin_folder = fs::read_dir(format!("../Skins/{}", &skinname)).await?;
+        let skin_folder_elements = if let Some(skin_folder_elements) = skin_folder.next_entry().await? {
+            skin_folder_elements
+        } else {
+            resp.edit(&ctx, |m| m.content("There was an error getting the folder containing the skin elements! Try re-exporting the skin!")).await?;
+            fs::remove_dir_all(Path::new(&format!("../Skins/{}", &skinname))).await?;
+            return Ok(());
+        };
+        if copy_all(skin_folder_elements.path(), Path::new(&format!("../Skins/{}", &skinname))).is_err() {
+            resp.edit(&ctx, |m| m.content("There was an error getting the folder containing the skin elements! Try re-exporting the skin!")).await?;
+            fs::remove_dir_all(Path::new(&format!("../Skins/{}", &skinname))).await?;
+            return Ok(());
+        } else {
+            if Path::new(&format!("../Skins/{}/skin.ini", &skinname)).exists() {
+                resp.edit(&ctx, |m| m.content("Found `skin.ini`!")).await?;
+                fs::remove_dir_all(skin_folder_elements.path()).await?;
+            } else {
+                resp.edit(&ctx, |m| m.content("There was an error getting the folder containing the skin elements! Try re-exporting the skin!")).await?;
+                fs::remove_dir_all(Path::new(&format!("../Skins/{}", &skinname))).await?;
+                return Ok(());
+            }
+        }
+    }
 
     let mut skins = fs::read_dir("../Skins/")
         .await
@@ -126,8 +164,50 @@ async fn addskin(ctx: &serenity::prelude::Context, msg: &Message) -> CommandResu
         }
     }
 
-    msg.reply(&ctx, format!("Added skin to list at index `{counter}`"))
-        .await?;
+    resp.edit(&ctx, |m| m.content(format!("Added skin to list at index `{counter}`"))).await?;
+
+    Ok(())
+}
+
+pub fn copy_all<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if std::fs::metadata(&dest).is_err() {
+            std::fs::create_dir_all(&dest)?;
+        }
+
+        for entry in std::fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        std::fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        unreachable!()
+                    }
+                }
+            }
+        }
+    }
 
     Ok(())
 }
