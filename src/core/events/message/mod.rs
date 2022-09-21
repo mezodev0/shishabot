152aa2ab4b72f7214b630b94855591b5dddd8e69
mezodev0 <mezodev02@gmail.com>
2022-09-21 05:50:25 -0700

    replay attachments that are not sent with `render` get handled properly now

--- src/core/events/message/mod.rs
+++ src/core/events/message/mod.rs
@@ -6,9 +6,21 @@ use crate::{core::Context, util::ChannelExt};
 
 pub async fn handle_message(ctx: Arc<Context>, msg: Message) {
     if let Some(attachment) = msg.attachments.first() {
+        let content = "Hey! Looks like you tried to send a replay\nPlease use **/render** as we have fully migrated to slash commands.";
         if matches!(attachment.filename.split('.').last(), Some("osr")) {
-            let content = "Hey! Looks like you tried to send a replay\nPlease use **/render** as we have fully migrated to slash commands.";
-            let _ = msg.error(&ctx, content).await;
+            let valid_input_channel = msg
+                .guild_id
+                .map(|f| ctx.guild_settings(f, |s| s.input_channels.contains(&msg.channel_id)));
+
+            match valid_input_channel {
+                Some(Some(true)) => {
+                    let _ = msg.error(&ctx, content).await;
+                }
+                Some(Some(false) | None) => {}
+                None => {
+                    let _ = msg.error(&ctx, content).await;
+                }
+            }
         }
     }
 }
