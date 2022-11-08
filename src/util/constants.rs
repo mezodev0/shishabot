3a8caf99f654a224056f713508890da8d8acbc6c
mezodev0 <mezodev02@gmail.com>
2022-11-08 03:20:52 -0800

    moved /skinlist to /skin list

--- src/util/constants.rs
+++ src/util/constants.rs
@@ -4,7 +4,7 @@ pub const RED: u32 = 0xE74C3C;
 
 // Error messages
 pub const GENERAL_ISSUE: &str = "Something went wrong, blame mezo";
-
+pub const NOT_OWNER: &str = "That command can only be used by the bot owner";
 // Misc
 pub const INVITE_LINK: &str = "https://discord.com/api/oauth2/authorize?client_id=867681950818893835&permissions=117824&scope=bot";
 pub const SHISHABOT_GITHUB: &str = "https://github.com/mezodev0/shishabot";
